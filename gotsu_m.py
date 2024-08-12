675e90ecd40d1df6a6c40a128580fe00af720e6e
mercur0us <105598670+mercur0us@users.noreply.github.com>
2024-08-12 16:01:07 +0800

    Update gotsu_m.py - slightly optimized, still not really working

--- gotsu_m.py
+++ gotsu_m.py
@@ -2,74 +2,75 @@
 class M_Gotsu:
     def __init__(self, px_count):
         self.px_count = px_count # array containing arrays of number of pixels of each value of each picture
-        self.t1 = None
-        self.t2 = None
+        self.t = None
         self.min_g = None
+        self.cumul_value = None # cumulative value of individual images
+        self.cumul_n = None
+        self.calc_cumul()
 
-    def calc_var(self, i, n_single, m, t1, t2):
-        sq_diff = 0
-        for j in range(t1, t2):
-            sq_diff += (self.px_count[i][j] - m)**2
-        if n_single-1 == 0:
-            return 0
-        else:
-            v = float(sq_diff / (n_single-1))
-            return v
-            
-    def calc_sum_mean_group(self, t1, t2):
-        total_value = 0 # total value of all images in group i
-        n = 0 # total number of pixels of all images in group i
-        m = [] # array of mean of group i of each image
-        n_arr = [] # array of number of pixels of group i of each image
-        v_arr = []
-        for i in range(len(self.px_count)):
-            value_single = 0
-            n_single = 0
+    def calc_cumul(self):
+        cumul_value_all_images = []
+        cumul_n_all_images = []
+        for i in self.px_count:
+            cumul_value = []
+            cumul_n = []
+            value = 0
+            n = 0
+            for j in range(0, 255):
+                value += i[j] * j
+                n += i[j]
+                cumul_value.append(value)
+                cumul_n.append(n)
+            cumul_value_all_images.append(cumul_value)
+            cumul_n_all_images.append(cumul_n)
+        self.cumul_value = cumul_value_all_images
+        self.cumul_n = cumul_n_all_images
+
+    def calc_f(self, m_arr, t1, t2): # calc f of all images in the group
+        f = 0
+        w = 0
+        v = 0
+        for i in range(len(m_arr)):
+            n = self.cumul_n[i][t2] - self.cumul_n[i][t1]
+            if self.cumul_n[i][254] != 0:
+                w = n / self.cumul_n[i][254]
+            sqd = 0
             for j in range(t1, t2):
-                if self.px_count[i][j] != 0:
-                    total_value += self.px_count[i][j] * j
-                    n += self.px_count[i][j]
-                    value_single += self.px_count[i][j] * j
-                    n_single += self.px_count[i][j]
+                sqd += (self.px_count[i][j] - m_arr[i])**2 
+            if n-1 != 0:
+                v = sqd / (n-1)
+            f += w*v
+        return f
+            
+    def calc_g(self, t1, t2): # calculate square diff and f of group between t1 and t2
+        v_tot = 0
+        n_tot = 0 
+        m_arr = [] # array of mean of indv images
+        sq_diff = 0
+        for i in self.cumul_value:
+            v = i[t2] - i[t1]
+            n = i[t2] - i[t1]
+            if n != 0:
+                m_arr.append(v/n)
+                v_tot += v
+                n_tot += n
+        if n_tot != 0:
+            m_tot = v_tot/n_tot
 
-            if n_single == 0:
-                m.append(0)
-                n_arr.append(0)
-                v_arr.append(0)
-            else:
-                mean = value_single / n_single
-                m.append(mean)
-                n_arr.append(n_single)
-                v = self.calc_var(i, n_single, mean, t1, t2)
-                v_arr.append(v)
-                
-        if n == 0:
-            return 0
-        else: 
-            m_tot = total_value / n
-            x = 0  # sum( (Mi_0 - Mi)**2 ) + f <- of all images
-            for i in range(len(m)):
-                w = n_arr[i] / n
-                x += ((m[i] - m_tot)**2 + (w*v_arr[i]))
-            return x
-    
-    def calc_G(self, t1, t2): # output value of g given t1, t2
-        sum_m1 = self.calc_sum_mean_group(0, t1)
-        sum_m2 = self.calc_sum_mean_group(t1, t2)
-        sum_m3 = self.calc_sum_mean_group(t2, 256)
-
-        g = sum_m1 + sum_m2 + sum_m3
+        for i in m_arr:
+            sq_diff += (i - m_tot)**2
+        f = self.calc_f(m_arr, t1, t2)
+        g = sq_diff + f
         return g
-    
+
     def find_thresh(self):
-        for t1 in range(0, 255):
-            for t2 in range(1, 256):
-                g_tot = self.calc_G(t1, t2)
-                if self.min_g == None or g_tot < self.min_g :
-                    self.min_g = g_tot
-                    self.t1 = t1
-                    self.t2 = t2
+        for t1 in range(0, 254):
+            for t2 in range(t1+1, 255): 
+                g = self.calc_g(0, t1) + self.calc_g(t1, t2) + self.calc_g(t2, 254)
+                if self.min_g == None or g < self.min_g:
+                    self.min_g = g
+                    self.t = [t1, t2]
         print("min g:" + str(self.min_g))
-        print("t1: " + str(self.t1))
-        print("t2: " + str(self.t2))
-        return [self.t1, self.t2] 
+        print("t1: " + str(self.t[0]))
+        print("t2: " + str(self.t[1]))
+        return self.t
