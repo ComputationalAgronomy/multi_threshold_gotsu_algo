1db67c1e75e734b27a7c786dd3858bbf901c5220
mercur0us <105598670+mercur0us@users.noreply.github.com>
2024-08-13 17:55:38 +0800

    Update gotsu_m.py - fixed and slightly optimized

--- gotsu_m.py
+++ gotsu_m.py
@@ -16,7 +16,7 @@ class M_Gotsu:
             cumul_n = []
             value = 0
             n = 0
-            for j in range(0, 255):
+            for j in range(len(i)):
                 value += i[j] * j
                 n += i[j]
                 cumul_value.append(value)
@@ -25,6 +25,8 @@ class M_Gotsu:
             cumul_n_all_images.append(cumul_n)
         self.cumul_value = cumul_value_all_images
         self.cumul_n = cumul_n_all_images
+        print(f"cumul_value length: {len(self.cumul_value[0])}, {len(self.cumul_value[1])}")
+        print(f"cumul_n length: {len(self.cumul_n[0])}, {len(self.cumul_n[1])}")
 
     def calc_f(self, m_arr, t1, t2): # calc f of all images in the group
         f = 0
@@ -32,8 +34,8 @@ class M_Gotsu:
         v = 0
         for i in range(len(m_arr)):
             n = self.cumul_n[i][t2] - self.cumul_n[i][t1]
-            if self.cumul_n[i][254] != 0:
-                w = n / self.cumul_n[i][254]
+            if self.cumul_n[i][255] != 0:
+                w = n / self.cumul_n[i][255]
             sqd = 0
             for j in range(t1, t2):
                 sqd += (self.px_count[i][j] - m_arr[i])**2 
@@ -45,31 +47,42 @@ class M_Gotsu:
     def calc_g(self, t1, t2): # calculate square diff and f of group between t1 and t2
         v_tot = 0
         n_tot = 0 
+        m_global = 0
         m_arr = [] # array of mean of indv images
         sq_diff = 0
-        for i in self.cumul_value:
+        for i, j in zip(self.cumul_value, self.cumul_n):
             v = i[t2] - i[t1]
-            n = i[t2] - i[t1]
+            n = j[t2] - j[t1]
             if n != 0:
                 m_arr.append(v/n)
                 v_tot += v
                 n_tot += n
         if n_tot != 0:
-            m_tot = v_tot/n_tot
+            m_global = v_tot/n_tot
 
         for i in m_arr:
-            sq_diff += (i - m_tot)**2
+            sq_diff += (i - m_global)**2
         f = self.calc_f(m_arr, t1, t2)
         g = sq_diff + f
         return g
 
     def find_thresh(self):
-        for t1 in range(0, 254):
-            for t2 in range(t1+1, 255): 
-                g = self.calc_g(0, t1) + self.calc_g(t1, t2) + self.calc_g(t2, 254)
-                if self.min_g == None or g < self.min_g:
+        for t1 in range(0, 256):
+            for t2 in range(t1+1, 256): 
+                g = 0
+                if self.min_g == None:
+                    g = self.calc_g(0, t1) + self.calc_g(t1, t2) + self.calc_g(t2, 255)
                     self.min_g = g
-                    self.t = [t1, t2]
+                else:
+                    g += self.calc_g(0, t1)
+                if g < self.min_g:
+                    g += self.calc_g(t1, t2) 
+                    if g < self.min_g: 
+                        g += self.calc_g(t2, 255)
+                        if g < self.min_g:
+                            self.min_g = g
+                            self.t = [t1, t2]
+        
         print("min g:" + str(self.min_g))
         print("t1: " + str(self.t[0]))
         print("t2: " + str(self.t[1]))
